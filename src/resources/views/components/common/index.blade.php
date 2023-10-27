37b50fcc044ef370322c082bf7a19a48a7501193
okina3 <takosos@icloud.com>
2023-10-27 13:27:46 +0900

    大枠の作成

---
+++ src/resources/views/components/common/index.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+   <div class="mb-2 h-[30vh] flex justify-between">
+      <section class="w-2/5 text-gray-600 border-gray-300 border rounded-lg overflow-hidden">
+         <h1 class="px-3 py-2 border-b text-lg bg-gray-200 border-slate-300">
+            タグ一覧
+         </h1>
+         <div class="p-3 h-full overflow-y-scroll overscroll-none">
+            タグ一覧エリア
+         </div>
+      </section>
+      <section class="w-3/5 ml-2 text-gray-600 border-gray-300 border rounded-lg overflow-hidden">
+         <h1 class="px-3 py-2 border-b text-lg bg-gray-200 border-slate-300">
+            メモ一覧
+         </h1>
+         <div class="p-3 h-full overflow-y-scroll overscroll-none">
+            メモ一覧エリア
+         </div>
+      </section>
+   </div>
+   {{ $slot }}
+</x-app-layout>
